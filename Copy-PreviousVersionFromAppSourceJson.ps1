049d2c80503b25f46ef78205e2f666bc2bf3e14e
James Pearson <james.pearson@norriq.com>
2022-09-21 13:56:33 +0100

    Copy-PreviousVersionFromAppSourceJson

---
+++ Copy-PreviousVersionFromAppSourceJson.ps1
@@ -0,0 +1,22 @@
+function Copy-PreviousVersionFromAppSourceJson {
+    param(
+        [string]$ProjectRoot = (Get-Location)
+    )
+
+    $AppSourceCop = Get-Content (Join-Path $ProjectRoot 'AppSourceCop.json') -Raw | ConvertFrom-Json
+    $AppJson = Get-Content (Join-Path $ProjectRoot 'app.json') -Raw | ConvertFrom-Json
+    $AppFileName = "$($AppJson.Publisher)_$($AppJson.Name)_$($AppSourceCop.version).app"
+
+    $Apps = Get-ChildItem -Path (Get-JPUtilsConfig -KeyName releasesPath) -Filter $AppFileName -Recurse | Select -Last 1
+    
+    if ($null -ne $Apps) {
+        $AppPath = $Apps.FullName
+        Copy-Item $AppPath (Join-Path $ProjectRoot '.alpackages')
+        "Copied $AppPath"
+    }
+    else {
+        "Couldn't find $AppFileName in the releases folder"
+    }
+}
+
+Export-ModuleMember .\Copy-PreviousVersionFromAppSourceJson

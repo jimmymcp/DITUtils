34949fce87fbc97103b8fadb8da34ee556a2d304
James Pearson <james.pearson@norriq.com>
2023-06-08 13:40:57 +0100

    Allow installing apps into global scope

--- Install-AppsFromFolder.ps1
+++ Install-AppsFromFolder.ps1
@@ -3,17 +3,28 @@ function Install-AppsFromFolder {
         [Parameter(Mandatory = $true)]
         [string]$ContainerName,
         [pscredential]$credential = (Get-Credential),
-        [string]$SourcePath = (Get-Location)
+        [string]$SourcePath = (Get-Location),
+        [switch]$GlobalScope
     )
 
     Sort-AppFilesByDependencies -appFiles (gci $SourcePath -Recurse -Filter *.app | % {
             $_.FullName
-        }) | % {
-        try {
-            Publish-BcContainerApp $ContainerName -appFile $_ -sync -syncMode Development  -upgrade -skipVerification -useDevEndpoint -credential $credential
+        }) -containerName $ContainerName | % {
+        if ($GlobalScope.IsPresent) {
+            try {
+                Publish-BcContainerApp $ContainerName -appFile $_ -sync -syncMode ForceSync -install -skipVerification
+            }
+            catch {
+                Publish-BcContainerApp $ContainerName -appFile $_ -sync -syncMode ForceSync -upgrade -skipVerification
+            }
         }
-        catch {
-            Publish-BcContainerApp $ContainerName -appFile $_ -sync -syncMode Development -install -skipVerification -useDevEndpoint -credential $credential
+        else {
+            try {
+                Publish-BcContainerApp $ContainerName -appFile $_ -sync -syncMode Development -upgrade -skipVerification -useDevEndpoint -credential $credential
+            }
+            catch {
+                Publish-BcContainerApp $ContainerName -appFile $_ -sync -syncMode Development -install -skipVerification -useDevEndpoint -credential $credential
+            }
         }
     }
 }

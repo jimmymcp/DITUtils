d788c69e4dff04b4d1be81e82b104e0e4cfc0388
James Pearson <james.pearson@totalenterprisesolutions.co.uk>
2024-04-10 09:46:45 +0100

    Use Release Notes field to build changelog

--- Get-ChangeLog.ps1
+++ Get-ChangeLog.ps1
@@ -1,31 +1,29 @@
 function Get-ChangeLog {
     param(
         [Parameter(Mandatory = $true)]
-        [int]$featureNo,
-        [switch]$includeDescription
+        [int]$featureNo
     )
 
     $result = @()
+    $firstChild = $true
 
     $feature = Invoke-AzureDevOpsAPI -url "wit/workitems/$featureNo" -method Get -params @{'$expand' = 'Relations' }
     $changedDate = $feature.fields.'System.ChangedDate'
 
-    # parent
-    $feature.relations | Where-Object rel -eq 'System.LinkTypes.Hierarchy-Reverse' | ForEach-Object {
-        $item = Invoke-AzureDevOpsAPI -url $_.url -params @{fields = 'System.Title,System.ChangedDate' }
-        $result += "### $($item.fields.'System.Title') $($feature.fields.'System.Title') ($($changedDate.Day)/$($changedDate.Month)/$($changedDate.Year))"
-    }
+    # version no. header
+    $result += "### $($feature.fields.'System.Title') ($($changedDate.Day)/$($changedDate.Month)/$($changedDate.Year))"
 
     #children
     $feature.relations | Where-Object rel -eq 'System.LinkTypes.Hierarchy-Forward' | ForEach-Object {
-        $item = Invoke-AzureDevOpsAPI -url $_.url -params @{fields = 'System.Title,System.Description' }
-        $result += "- [$($item.id)](https://dev.azure.com/TES365/TES%20IP/_workitems/edit/$($item.id)): $($item.fields.'System.Title')"
-        if ($includeDescription) {
-            $result += $item.fields.'System.Description'
-            $result += ''
+        $item = Invoke-AzureDevOpsAPI -url $_.url -params @{fields = 'System.Title,Custom.ReleaseNotes' }
+        if (!($firstChild)) {
             $result += '<hr/>'
             $result += ''
         }
+        $result += "- [$($item.id)](https://dev.azure.com/TES365/TES%20IP/_workitems/edit/$($item.id)): $($item.fields.'System.Title')"
+        $result += $item.fields.'Custom.ReleaseNotes'
+        $result += ''
+        $firstChild = $false
     }
 
     $result

a62cc6aa17e015dd52c98accc72735fad4ac54fc
James Pearson <james.pearson@tes365.com>
2024-10-02 10:58:29 +0100

    Update to New-DocInvitation

--- New-DocsInvitation.ps1
+++ New-DocsInvitation.ps1
@@ -3,7 +3,9 @@ function New-DocsInvitation {
         [string]$email
     )
 
-    az staticwebapp users invite --authentication-provider AAD --domain "docs.tes365.com" --name TES-IP-Docs --roles "reader" --invitation-expiration-in-hours 168 --user-details $email
+    $invitation = az staticwebapp users invite --authentication-provider AAD --domain "docs.tes365.com" --name TES-IP-Docs --roles "reader" --invitation-expiration-in-hours 744 --user-details $email
+    $invitation.invitationUrl | Set-Clipboard
+    $invitation
 }
 
 Export-ModuleMember -Function New-DocsInvitation

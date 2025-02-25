1a3f46f1dbe93d905c21f3dde5b7f433232e8aa6
James Pearson <james.pearson@tes365.com>
2025-02-25 11:05:13 +0000

    Sort release branches as versions

--- Get-ReleaseBranchName.ps1
+++ Get-ReleaseBranchName.ps1
@@ -1,5 +1,13 @@
 function Get-ReleaseBranchName {
-    (git branch | Where-Object {$_.TrimStart().StartsWith('release/')} | Sort-Object -Descending | Select-Object -First 1).Trim()
+    $releaseBranches = @()
+    git branch | Where-Object {$_.TrimStart().StartsWith('release/')} | ForEach-Object {
+        $releaseBranch = @{
+            Name = $_.Trim()
+            Version = [Version]::Parse($_.Trim().TrimStart('release/'))
+        }
+        $releaseBranches += $releaseBranch
+    }
+    ($releaseBranches | Sort-Object -Property Version -Descending | Select-Object -First 1).Name
 }
 
 Export-ModuleMember -Function Get-ReleaseBranchName

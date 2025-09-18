f72f874b3f4420d8a34bc6783ad6521d6d4ad88f
James Pearson <james.pearson@tes365.com>
2025-09-18 16:23:13 +0100

    Add search parameter to Get-Customers

--- Get-Customers.ps1
+++ Get-Customers.ps1
@@ -1,5 +1,13 @@
 function Get-Customers {
-    Get-Content (Join-Path ([Environment]::GetFolderPath('userprofile')) "Downloads\Customers.csv") -Raw | ConvertFrom-Csv -Header ('MicrosoftID', 'CompanyName', 'PrimaryDomainName', 'Relationship', 'Tags') | Select-Object -Skip 1
+    param(
+        [Parameter(Position = 1, Mandatory=$false)]
+        [string]$search
+    )
+    $customers = Get-Content (Join-Path ([Environment]::GetFolderPath('userprofile')) "Downloads\Customers.csv") -Raw | ConvertFrom-Csv -Header ('MicrosoftID', 'CompanyName', 'PrimaryDomainName', 'Relationship', 'Tags') | Select-Object -Skip 1
+    if ($search) {
+        $customers = $customers | Where-Object {$_.PrimaryDomainName -like "*$($search)*" -or $_.CompanyName -like "*$($search)*"}
+    }
+    $customers
 }
 
 Export-ModuleMember -Function Get-Customers
